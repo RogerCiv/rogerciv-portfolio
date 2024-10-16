cf06c3ab6ddaf6dd63ecada0ae02d47e610565a6
RogerCiv <rogeliocivantos@gmail.com>
2024-10-16 19:45:35 +0200

    add vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
 
+import vercel from '@astrojs/vercel/serverless';
+
+
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: vercel()
 });
