2ec6731fd0ac4e2570e95f29cef6f726fa8f83c3
RogerCiv <rogeliocivantos@gmail.com>
2025-01-09 18:31:30 +0100

    Add Vercel Analytics integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,9 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  })
 });
